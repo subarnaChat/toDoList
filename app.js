b09ac16e5c1db19e616d8eddc31be8d5671fcac8
@subarnaChat <subarna.chatterjee1998@gmail.com>
2022-06-24 17:38:58 +0530

    Added Procfile and gitignore

--- app.js
+++ app.js
@@ -125,6 +125,11 @@ app.post("/delete",function(req,res){
 
 });
 
-app.listen(3000,function(req,res){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port,function(req,res){
   console.log("running");
 });
